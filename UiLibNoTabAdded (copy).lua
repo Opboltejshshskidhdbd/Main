634bfe8ae14d96818c16db12a1a9fe8791b2a3cc
Opboltejshshskidhdbd <106669179+Opboltejshshskidhdbd@users.noreply.github.com>
2024-09-14 18:41:18 +0530

    Update UiLibNoTabAdded (copy).lua

--- UiLibNoTabAdded (copy).lua
+++ UiLibNoTabAdded (copy).lua
@@ -380,7 +380,7 @@ local glowPulse = Instance.new("Frame")
 
 Toggle.Name = "Toggle"
 Toggle.Parent = ScrollingFrame
-Toggle.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Toggle.BackgroundTransparency = 0
 Toggle.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Toggle.BorderSizePixel = 0
